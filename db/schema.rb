9cb2e38c189129591f22bb05e5a8eb7a01bdda38
Takahashi Aoi <takahashi.a.at@m.titech.ac.jp>
2017-06-02 13:27:50 +0900

    scaffolding機能により、projectsテーブルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124071159) do
+ActiveRecord::Schema.define(version: 20170602042046) do
 
   create_table "bookmarks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -63,6 +63,12 @@ ActiveRecord::Schema.define(version: 20170124071159) do
   end
 
   create_table "patch_authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
